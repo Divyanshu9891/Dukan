7a9e179bff982fcfccddbb1de6ae3d708826dcda
Divyanshu <divyanshu.jangid9891@gmail.com>
2026-04-17 21:37:12 +0530

    FEAT:AboutPage

---
+++ src/Components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our company! We are a team of passionate developers dedicated to creating amazing web applications.</p> 
+    </div>
+  )
+}
